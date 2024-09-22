56fea10af8c40f9480d179df44449b1710f3f655
KwanJunEr <jonastheitguy@gmail.com>
2024-09-22 16:39:10 +0800

    commit 6them

---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { BrowserRouter as Router } from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import {PrivyProvider} from '@privy-io/react-auth';
+
+
+const root = createRoot(document.getElementById('root'))
+
+root.render(
+
+)
