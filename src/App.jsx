ad7fd4e67c1de04f1d7f312ae99de0b3bb2f62d4
KwanJunEr <jonastheitguy@gmail.com>
2024-09-22 20:35:40 +0800

    Made some changes

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,14 @@ const App = ()=>{
 
   return(
     <div className="relative flex min-h-screen flex-row bg-[#13131a] p-4">
-
+        <h1 className="font-bold text-red-600">Asdasd</h1>
+        asdasdasd
+        asdasdasdas
+        asda
     </div>
   )
 }
+
+export default App;
 
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,29 +1,25 @@
-import React from 'react'
-import { BrowserRouter as Router } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import {PrivyProvider} from '@privy-io/react-auth';
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter as Router } from 'react-router-dom';
+import App from './App.jsx';
+import './index.css';
+import { PrivyProvider } from '@privy-io/react-auth';
 
-
-const root = createRoot(document.getElementById('root'))
+const root = createRoot(document.getElementById('root'));
 
 root.render(
+  <React.StrictMode>
     <PrivyProvider
       appId="cm0p24zs000spq0manegekhf8"
       config={{
-        // Customize Privy's appearance in your app
-       
         appearance: {
           theme: 'dark',
-         
         },
-        // Create embedded wallets for users who don't have a wallet
-       
       }}
     >
-       <Router>
-         <App/>
-       </Router>
-     
+      <Router>
+        <App />
+      </Router>
     </PrivyProvider>
-)
+  </React.StrictMode>
+);
