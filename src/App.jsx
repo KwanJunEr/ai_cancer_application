b1502fe4578c42e0df354e21e3bdc8dab4965980
KwanJunEr <jonastheitguy@gmail.com>
2024-09-22 19:27:00 +0800

    commit new changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
+import React from "react"
+import {Route,Routes, useNavigate} from 'react-router-dom'
 
-function App() {
+const App = ()=>{
 
+  return(
+    <div className="relative flex min-h-screen flex-row bg-[#13131a] p-4">
 
-  return (
-    <>
-     
-    </>
+    </div>
   )
 }
 
-export default App
+
--- src/main.jsx
+++ src/main.jsx
@@ -8,5 +8,22 @@ import {PrivyProvider} from '@privy-io/react-auth';
 const root = createRoot(document.getElementById('root'))
 
 root.render(
-
+    <PrivyProvider
+      appId="cm0p24zs000spq0manegekhf8"
+      config={{
+        // Customize Privy's appearance in your app
+       
+        appearance: {
+          theme: 'dark',
+         
+        },
+        // Create embedded wallets for users who don't have a wallet
+       
+      }}
+    >
+       <Router>
+         <App/>
+       </Router>
+     
+    </PrivyProvider>
 )
