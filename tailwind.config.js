45fedb222366d5fa73960030b514dbff701f3ec6
KwanJunEr <jonastheitguy@gmail.com>
2024-09-22 22:32:39 +0800

    Completed the Sidebar Component

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,18 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        epilogue: ["Epilogue", "sans-serif", "Poppins"],
+      },
+      boxShadow: {
+        secondary: "10px 10px 20px rgba(2, 2, 2, 0.25)",
+      },
+      colors: {
+        mainBackgroundColor: "#0D1117",
+        columnBackgroundColor: "#161C22",
+      },
+    },
   },
-  plugins: [],
+  plugins: [require("preline/plugin")],
 }
